d95d9c845da03bce20a4a7ae7766f01f3f26fdc2
Melanie Phipps McCord <mpmccord@wm.edu>
2024-02-18 17:39:59 -0500

    Added backend, converted everything to backend and frontend folders, removed GitHub Pages deployment.

--- frontend/src/routes/MySideNav.js
+++ frontend/src/routes/MySideNav.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, createContext} from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
+import { GiPoliceOfficerHead } from "react-icons/gi";
 import {FaFolder, FaCog, FaWrench, FaChartArea, FaTable, FaSmileWink, FaChartLine, FaChartBar, FaCity, FaCat} from "react-icons/fa";
 import { NavItem } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
@@ -20,29 +21,28 @@ function changeIcon(myIcon) {
 export function MySideNav() {
     return (
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div class="sidebar-brand-icon rotate-n-15">
+            <LinkContainer to="/">
+            <div className="sidebar-brand d-flex align-items-center justify-content-center">
+            <div className="sidebar-brand-icon rotate-n-15">
                 <FaCity/>
             </div>
-            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-        </a>
+            <div class="sidebar-brand-text mx-3">Crime in Chicago</div>
+        </div>
+            </LinkContainer>
             <hr className="sidebar-divider" />
             <div className="sidebar-heading">Overview</div>
             <LinkContainer to="/">
-                    <Nav.Link> <FaCat/> {"Home"}</Nav.Link>
+                    <Nav.Link> <GiPoliceOfficerHead/> {"Home"}</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to="dataset">
                     <Nav.Link> <FaTable/> {"Datasets"}</Nav.Link>
                 </LinkContainer>
-            <hr className="sidebar-divider" />
+            <hr className="sidebar-divider"></hr>
+            <div className="sidebar-heading">Pages</div>
             <LinkContainer to="example-page">
                     <Nav.Link> <FaCat/> {"Cat Example"}</Nav.Link>
                 </LinkContainer>
-            <div className="sidebar-heading">Addons</div>
             <hr className="sidebar-divider d-none d-md-block" />
-            <div className="text-center d-none d-md-inline">
-                <button className="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
         </ul>
     );
 }
