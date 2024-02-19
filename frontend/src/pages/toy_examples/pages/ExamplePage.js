c4ffad8651eed8f75fdcb2e909bda8fc93a27e6f
Melanie Phipps McCord <mpmccord@wm.edu>
2024-02-18 22:24:07 -0500

    Fixed Cat Example

--- frontend/src/pages/toy_examples/pages/ExamplePage.js
+++ frontend/src/pages/toy_examples/pages/ExamplePage.js
@@ -9,8 +9,7 @@ export function ExamplePage() {
         <Container width={"50%"}>
           <Row>
             <Col id="col01"><img src={Cat2} alt="Cat" />;</Col>
-              <Col><img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                        src={Cat3}/></Col>
+              <Col><img src={Cat3}/></Col>
           </Row>
         </Container>
       </div>
